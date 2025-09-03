964606d0f7acd5cb9651de9193dedd89d2c14c3c
Nicolas Meza Pastor <nicolayuss369@gmail.com>
2025-09-03 04:45:01 -0500

    Cambio en Imagenes

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -1,11 +1,10 @@
 export const products = [
-  { id: 1, name: "No Me Abandones", price: 59.9, category: "Ropa", image: "./public/PoloCelesteGod.jpg" },
-  { id: 2, name: "Escala", price: 199.9, category: "Ropa", image: "./public/PoloCeleste.jpg" },
-  { id: 3, name: "Explora Celeste Sudadera", price: 149.9, category: "Ropa", image: "./public/ExploraCelesteSudadera.jpg" },
-  { id: 4, name: "Bufanda Celeste", price: 149.9, category: "Ropa", image: "./public/BufandaCeleste.jpg" },
-  { id: 5, name: "Recuerdo de la Montaña Pin", price: 89.9, category: "Accesorios", image: "./public/PinCeleste.jpg" },
-  { id: 6, name: "Fresa Voladora Pin", price: 39.9, category: "Accesorios", image: "./public/CollarFresaCeleste.jpg" },
-  { id: 7, name: "Fresa Dorada Pin", price: 39.9, category: "Accesorios", image: "./public/AccesorioCeleste.jpg" },
-  { id: 8, name: "Recuerdo de la Montaña Pin", price: 89.9, category: "Accesorios", image: "./public/PinCeleste.jpg" },
-
-]
+  { id: 1, name: "No Me Abandones", price: 59.9, category: "Ropa", image: "/PoloCelesteGod.jpg" },
+  { id: 2, name: "Escala", price: 199.9, category: "Ropa", image: "/PoloCeleste.jpg" },
+  { id: 3, name: "Explora Celeste Sudadera", price: 149.9, category: "Ropa", image: "/ExploraCelesteSudadera.jpg" },
+  { id: 4, name: "Bufanda Celeste", price: 149.9, category: "Ropa", image: "/BufandaCeleste.jpg" },
+  { id: 5, name: "Recuerdo de la Montaña Pin", price: 89.9, category: "Accesorios", image: "/PinCeleste.jpg" },
+  { id: 6, name: "Fresa Voladora Pin", price: 39.9, category: "Accesorios", image: "/CollarFresaCeleste.jpg" },
+  { id: 7, name: "Fresa Dorada Pin", price: 39.9, category: "Accesorios", image: "/AccesorioCeleste.jpg" },
+  { id: 8, name: "Recuerdo de la Montaña Pin", price: 89.9, category: "Accesorios", image: "/PinCeleste.jpg" },
+];
